6b473370290e8843d1c18eb8bf0dcb105fa2ab91
Adinath <adinath0632.com>
2025-07-20 23:02:28 +0530

    buttons fixed

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
-function buttons (){
-    let calc = 0;
-    let max = 9;
-const display = document.getElementById("count");
-console.log(display.textContent);
-
-
 const increment = document.getElementById('increment')
+const display = document.getElementById("count");
+let max = 9;
+let calc = 0;
+const decrement = document.getElementById('decrement')
+
+function btnAnimation(){
+    display.textContent = calc;
+
+    if (calc === 0) {
+        gsap.to(decrement,{
+            scale: 0,
+            opacity: 0,
+            duration: 0.3,
+        })
+    } else {
+        gsap.to(decrement,{
+            scale: 1,
+            opacity: 1,
+            duration: 0.3,
+        })
+    }
+}
+
+
+
+
+function buttons (){
+
+
 increment.addEventListener('click', () => {
-if (calc <max) {
-            ++calc
-        display.innerText = calc
+if (calc < max) {
+            calc++
+            btnAnimation()
 }
 })
 
-const decrement = document.getElementById('decrement')
 decrement.addEventListener('click',()=>{
     if (calc > 0) {
         calc--;
-    display.textContent = calc
+        btnAnimation()
     }
 })
 const reset = document.getElementById('reset')
 reset.addEventListener('click',()=>{
     calc = 0;
-    display.textContent = 0
+    display.textContent = 0;
+    btnAnimation()
 })
 }
 buttons()
-
