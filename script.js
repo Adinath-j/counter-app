cd33ce8f2a20cb903a012aef34e63e0f450c91a7
Adinath <adinath0632.com>
2025-07-20 19:40:41 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function buttons (){
+    let calc = 0;
+
+const display = document.getElementById("count");
+console.log(display.textContent);
+
+
+const increment = document.getElementById('increment')
+increment.addEventListener('click', () => {
+    calc++;
+    display.textContent = calc
+})
+
+const decrement = document.getElementById('decrement')
+decrement.addEventListener('click',()=>{
+    calc--;
+    display.textContent = calc
+})
+const reset = document.getElementById('reset')
+reset.addEventListener('click',()=>{
+    calc = 0;
+    display.textContent = 0
+})
+}
+buttons()
